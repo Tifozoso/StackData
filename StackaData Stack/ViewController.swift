4e1b0cd9a94d08ed66be4d781e721af18e337c1c
Tifozoso <mfranks@gmail.com>
2017-09-12 18:11:02 -0700

    Initial Commit

---
+++ StackaData Stack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackaData Stack
+//
+//  Created by Michael Franks on 9/12/17.
+//  Copyright © 2017 Popcorn Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
